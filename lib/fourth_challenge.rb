0c3940b0b8e5dcc5f75365c3ab3de44e56bb3d1d
Andre Santiago <the.asantiagojr@gmail.com>
2018-06-20 09:52:51 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,11 +22,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {
-          :name => "Steven",
+        { :name => "Steven",
           :age => "30",
-          :attitude => "confused"
-        },
+          :attitude => "confused" },
         { :name => "Nurse",
           :age => "44",
           :attitude => "worried" }
